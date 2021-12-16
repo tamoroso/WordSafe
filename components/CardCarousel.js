945346761cd1dbb5802dc881c13f0b12d0bd873f
tamoroso <amorosothomas@orange.fr>
2021-12-16 17:08:37 +0100

    Wheel event up and down detection

--- components/CardCarousel.js
+++ components/CardCarousel.js
@@ -16,34 +16,57 @@ function determineClasses(indexes, cardIndex) {
 
 const CardCarousel = () => {
   const [indexes, setIndexes] = useState({
-    previousIndex: 0,
+    previousIndex: Object.keys(passwords).length - 1,
     currentIndex: 0,
     nextIndex: 1,
   });
   const carousel = useRef();
 
-  const handleCardTransition = useCallback(() => {
-    if (indexes.currentIndex >= Object.keys(passwords).length - 1) {
-      setIndexes({
-        previousIndex: Object.keys(passwords).length - 1,
-        currentIndex: 0,
-        nextIndex: 1,
-      });
-    } else {
-      setIndexes((prevState) => ({
-        previousIndex: prevState.currentIndex,
-        currentIndex: prevState.currentIndex + 1,
-        nextIndex:
-          prevState.currentIndex + 2 === Object.keys(passwords).length
-            ? 0
-            : prevState.currentIndex + 2,
-      }));
-    }
-  }, [indexes.currentIndex]);
+  const handleCardTransition = useCallback(
+    (event) => {
+      console.log(event.deltaY);
+      console.log(indexes);
+      if (
+        indexes.currentIndex >= Object.keys(passwords).length - 1 &&
+        event.deltaY > 0
+      ) {
+        setIndexes({
+          previousIndex: Object.keys(passwords).length - 1,
+          currentIndex: 0,
+          nextIndex: 1,
+        });
+      } else if (indexes.currentIndex - 1 < 0 && event.deltaY < 0) {
+        setIndexes({
+          previousIndex: Object.keys(passwords).length - 2,
+          currentIndex: Object.keys(passwords).length - 1,
+          nextIndex: 0,
+        });
+      } else if (event.deltaY > 0) {
+        setIndexes((prevState) => ({
+          previousIndex: prevState.currentIndex,
+          currentIndex: prevState.currentIndex + 1,
+          nextIndex:
+            prevState.currentIndex + 2 === Object.keys(passwords).length
+              ? 0
+              : prevState.currentIndex + 2,
+        }));
+      } else if (event.deltaY < 0) {
+        setIndexes((prevState) => ({
+          previousIndex:
+            prevState.currentIndex - 2 === -1
+              ? Object.keys(passwords).length - 1
+              : prevState.currentIndex - 2,
+          currentIndex: prevState.currentIndex - 1,
+          nextIndex: prevState.currentIndex,
+        }));
+      }
+    },
+    [indexes]
+  );
 
   useEffect(() => {
     const ul = carousel.current;
-    const debouncedCardTransition =  _.debounce(handleCardTransition, 250)
+    const debouncedCardTransition = _.debounce(handleCardTransition, 200);
     ul.addEventListener("wheel", debouncedCardTransition);
 
     return () => {
